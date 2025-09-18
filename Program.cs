1b86140f03140ad34cac91615e14093702678529
fannybillefalt <fannybillefalt@gmail.com>
2025-09-18 12:29:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace NumbersGame
+{
+    internal class Program
+    {
+        public static void WelcomeMessage()//Welcome messages in an own method, using void because no return.
+        {
+            Console.WriteLine("Välkommen! Jag tänker på ett nummer. Kan du gissa vilket? Du får fem försök.");
+        }
+
+        static void Main(string[] args)
+        {
+            Random random = new Random(); //Random class with Next method that randomly generates a number between 1-20.
+            int randomNumber = random.Next(1, 21);
+            bool isCorrectGuess = false; //Bool generates true or false answer.
+            int guesses = 0;
+
+            WelcomeMessage(); //Calling my method.
+
+            while (!isCorrectGuess)//while loop with condition if NOT false, keep going.
+            {
+                int.TryParse(Console.ReadLine(), out int guess);//Try Parse to make sure no error.
+                guesses++; //counter
+
+                //if statement with two terms, either IF to high of ELSE IF to low. Then un ELSE if neither of the terms applies.
+                if (guess > randomNumber)
+                {
+                    Console.WriteLine("Tyvärr du gissade för högt!");
+                }
+                else if (guess < randomNumber)
+                {
+                    Console.WriteLine("Tyvärr du gissade för lågt!");
+                }
+                else
+                {
+                    Console.WriteLine("Wohoo! Du gjorde det!");
+                    isCorrectGuess = true;//Bool becomes true.
+                }
+
+                if (guesses == 5 && !isCorrectGuess)//a separated IF with two terms if guesses is 5 and NOT false.
+                {
+                    Console.WriteLine("Tyvärr du lyckades inte gissa talet på fem försök!");
+                }
+
+            }
+
+            Console.WriteLine("Du vann!");
+
+            Console.ReadKey();
+        }
+    }
+}
